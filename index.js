3559439467c18292be9bc62706b61a05e05287da
Rokas Anisas <rokas@10speed.lt>
2022-09-27 14:09:18 +0300

    update rules

--- index.js
+++ index.js
@@ -40,6 +40,8 @@ module.exports = {
     'react/react-in-jsx-scope': 0,
     'react/prop-types': 0,
     'react/no-unescaped-entities': 0,
+    'react/display-name': 0,
+    'jsx-a11y/no-autofocus': 0,
   },
 
   overrides: [
